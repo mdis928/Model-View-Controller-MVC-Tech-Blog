951d0465dea6f63b744d2bc9d7571a4d3a7a1052
Michael DiSanto <mdisanto928@gmail.com>
2021-06-01 12:41:23 -0400

    added code. Still learning and playing around with code

---
+++ controllers/home-routes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { Dashboard  } = require('../models');
+
+// GET all Dashboard Posts for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbDashboardData = await Dashboard.findAll({
+      include: [
+        {
+          // model: Painting,
+          // attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const dashboard = dbDashboardData.map((gallery) =>
+      gallery.get({ plain: true })
+    );
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    res.render('homepage', {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/gallery/:id', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            'id',
+            'title',
+            'artist',
+            'exhibition_date',
+            'filename',
+            'description',
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    // Send over the 'loggedIn' session variable to the 'gallery' template
+    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render('login');
+});
+
+module.exports = router;
