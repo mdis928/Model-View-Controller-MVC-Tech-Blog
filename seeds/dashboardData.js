8699204cd2d5ac357731db394b8b9caa7b783660
Michael DiSanto <mdisanto928@gmail.com>
2021-06-02 11:23:42 -0400

    this homework is tough. Trying to simplify this

--- seeds/dashboardData.js
+++ seeds/dashboardData.js
@@ -1,28 +1,26 @@
-const { Gallery } = require('../models');
+const { BlogPosts } = require('../models');
 
-const gallerydata = [
+const BlogPostsData = [
   {
-    name: 'Printemps',
-    starting_date: 'April 20, 2021 07:00:00',
-    ending_date: 'June 21, 2021 17:00:00',
+    name: 'Learning MVC',
+    content: 'MVC is an architectural pattern that structures a codebase in three distinct sections, according to a software design philosophy known as the **separation of concerns**.'
+    // starting_date: 'April 20, 2021 07:00:00',
+    // ending_date: 'June 21, 2021 17:00:00',
   },
   {
-    name: 'Sommer',
-    starting_date: 'June 22, 2021 09:00:00',
-    ending_date: 'September 22, 2021 22:00:00',
+    name: 'Learning how to use Insomnia with ORM',
+    content: "I used Inomnia to use GET, PUT, POST, DELETE routes. I connect it with the MySQL and I see the data in the db!!"
+    // starting_date: 'June 22, 2021 09:00:00',
+    // ending_date: 'September 22, 2021 22:00:00',
   },
   {
-    name: 'Herfst',
-    starting_date: 'September 23, 2021 08:30:00',
-    ending_date: 'December 21, 2021 20:30:00',
-  },
-  {
-    name: 'Invierno',
-    starting_date: 'December 22, 2020 11:00:00',
-    ending_date: 'March 19, 2021 19:00:00',
+    name: 'Learning MySQL',
+    content: "MySQL was easy to understand as I used excel everyday. I look at tables and fields that make up the spreadsheet? "
+    // starting_date: 'September 23, 2021 08:30:00',
+    // ending_date: 'December 21, 2021 20:30:00',
   },
 ];
 
-const seedGallery = () => Gallery.bulkCreate(gallerydata);
+const seedBlogPosts = () => BlogPosts.bulkCreate(BlogPostsData);
 
-module.exports = seedGallery;
+module.exports = seedBlogPosts;
