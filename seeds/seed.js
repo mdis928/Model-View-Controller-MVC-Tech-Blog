1b6fa5cfba8e0cf80a4ebeecd7a5c8d5bebf2c8b
Michael DiSanto <mdisanto928@gmail.com>
2021-06-03 10:17:26 -0400

    Created and added code for seed.js in seeds folder

---
+++ seeds/seed.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/connection');
+const { User, Comment, NewPost } = require('../models');
+
+const userData = require('./userData.json');
+const commentData = require('./commentData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+  
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    }); 
+    console.log('\n----- USER DATA SYNCED -----\n')
+
+    await Comment.bulkCreate(commentData, {}); 
+      console.log('\n----- COMMENTS DATA SYNCED -----\n')
+  
+      await NewPost.bulkCreate(postData, {}); 
+      console.log('\n----- TECH BLOG POST DATA SYNCED -----\n')
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();
