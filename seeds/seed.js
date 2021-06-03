8ef5354c603aaedd377d44262a000ad2745b7d80
Michael DiSanto <mdisanto928@gmail.com>
2021-06-03 13:06:18 -0400

    updated package

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,7 @@ const { User, Comment, NewPost } = require('../models');
 
 const userData = require('./userData.json');
 const commentData = require('./commentData.json');
-const postData = require('./postData.json');
+const newPostData = require('./newPostData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -18,7 +18,7 @@ const seedDatabase = async () => {
     await Comment.bulkCreate(commentData, {}); 
       console.log('\n----- COMMENTS DATA SYNCED -----\n')
   
-    await NewPost.bulkCreate(postData, {}); 
+    await NewPost.bulkCreate(newPostData, {}); 
       console.log('\n----- TECH BLOG POST DATA SYNCED -----\n')
   
     process.exit(0);
