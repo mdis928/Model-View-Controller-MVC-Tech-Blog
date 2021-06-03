143c2a9c30d2ce52e73c76c824db7b965404c2e2
Michael DiSanto <mdisanto928@gmail.com>
2021-06-02 21:50:39 -0400

    dashboard.js is not needed. Code added to all models. Next step is to look at APIs and test the routing

--- models/NewPost.js
+++ models/NewPost.js
@@ -1,1 +1,43 @@
-NewPost.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class NewPost extends Model {}
+
+NewPost.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.STRING,
+      },
+      date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'NewPost',
+    }
+  );
+  
+  module.exports = NewPost;
--- models/index.js
+++ models/index.js
@@ -3,24 +3,22 @@ const User = require('./User');
 // NewPost is on the dashboard
 const NewPost = require('./NewPost');
 
-// In the dashboard, you can add a new post and update as well
-const Dashboard = require('./Dashboard');
 
 // To add comments, you need to be in the homescreen
 const Comment = require('./Comment');
 
 
 
-User.hasMany(NewPost, {
-    foreignKey: 'user_id',
-});
-
-Dashboard.hasMany(NewPost, {
-    foreignKey: "user_id"
-});
+NewPost.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
 NewPost.hasMany(Comment, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, NewPost, Dashboard, Comment};
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, NewPost, Comment};
