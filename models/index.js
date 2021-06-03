b3cd8099a8eb3b02060e8e5d05fcf924b7c67337
Michael DiSanto <mdisanto928@gmail.com>
2021-06-02 20:48:18 -0400

    Working on models. Added code to index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,14 @@
-index.js
+const User = require('./User');
+const NewPost = require('./NewPost');
+const Dashboard = require('./Dashboard');
+const Comment = require('./Comment');
+
+Dashboard.belongsTo(NewPost, {
+    foreignKey: 'Dashboard_id',
+});
+
+NewPost.hasMany(Comment, {
+    foreignKey: "NewPost_id"
+});
+
+module.exports = { User, NewPost, Dashboard, Comment};
