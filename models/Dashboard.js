5b7370ddef85b1232aad6c731e5f2154119e7df3
Michael DiSanto <mdisanto928@gmail.com>
2021-06-02 21:21:04 -0400

    updated index.js for models

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -1,1 +1,21 @@
-Dashboard.js
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require = ('../config/connection');
+
+class Dashboard extends Model {}
+
+Dashboard.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+    }
+
+
+
+
+
+)
--- models/index.js
+++ models/index.js
@@ -1,14 +1,21 @@
 const User = require('./User');
+
+// NewPost is in the homescreen
 const NewPost = require('./NewPost');
+
 const Dashboard = require('./Dashboard');
 const Comment = require('./Comment');
 
-Dashboard.belongsTo(NewPost, {
-    foreignKey: 'Dashboard_id',
+User.hasMany(NewPost, {
+    foreignKey: 'user_id',
+});
+
+Dashboard.hasMany(NewPost, {
+    foreignKey: "user_id"
 });
 
 NewPost.hasMany(Comment, {
-    foreignKey: "NewPost_id"
+    foreignKey: 'user_id'
 });
 
 module.exports = { User, NewPost, Dashboard, Comment};
