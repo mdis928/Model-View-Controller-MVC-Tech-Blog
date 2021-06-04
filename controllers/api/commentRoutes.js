19d51a2ce1c86c413b73854388f90c63e58251d4
Michael DiSanto <mdisanto928@gmail.com>
2021-06-04 08:21:57 -0400

    comment routes have updated. Looks like they are working. Can play around with this more later on. Proceeding with new post and user data routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+
+// Find all comments
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(commentData);
+    }   catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 // To create a new comment
 router.post('/', async (req, res) => {
     try {
@@ -15,13 +30,35 @@ router.post('/', async (req, res) => {
     }
 });
 
+
+router.put('/:id', async (req, res) => {
+    // update a comment by its `id` value
+    try {
+      const commentData = await Comment.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!commentData) {
+      res.status(404).json({ message: "No category can be updated with that id"});
+      return;
+    }  
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
 // To delete a comment
 router.delete('/:id', async (req, res) => {
     try {
       const commentData = await Comment.destroy({
           where: {
               id: req.params.id,
-              user_id: req.session.user_id,
           },
       });
 
