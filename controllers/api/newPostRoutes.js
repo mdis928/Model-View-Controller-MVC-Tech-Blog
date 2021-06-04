d3920c5ef1430c53d6309283dc9be016844fed01
Michael DiSanto <mdisanto928@gmail.com>
2021-06-04 08:44:32 -0400

    posts routes are working. Can play with more later. Looking at user

--- controllers/api/newPostRoutes.js
+++ controllers/api/newPostRoutes.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const { NewPost } = require('../../models');
 
+// Find all posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await NewPost.findAll({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(postData);
+    }   catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 // To create a new post in the tech blog
 router.post('/', async (req, res) => {
     try {
@@ -9,11 +23,33 @@ router.post('/', async (req, res) => {
             user_id: req.session.user_id,
         });
 
-        res.status(200).json(NewPost);
+        res.status(200).json(NewPostData);
     }   catch (err) {
         res.status(400).json(err);
     }
 });
+
+
+// update a post by its `id` value
+router.put('/:id', async (req, res) => {
+    try {
+      const postData = await NewPost.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!commentData) {
+      res.status(404).json({ message: "No category can be updated with that id"});
+      return;
+    }  
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 // To delete a post
 router.delete('/:id', async (req, res) => {
@@ -21,7 +57,6 @@ router.delete('/:id', async (req, res) => {
       const postData = await NewPost.destroy({
           where: {
               id: req.params.id,
-              user_id: req.session.user_id,
           },
       });
 
